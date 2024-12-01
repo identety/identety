e98c486ccdf34e721408e48dbe9037cc72021c66
kingrayhan <rayhan095@gmail.com>
2024-12-01 09:29:49 +0600

    unit test client service

--- src/modules/client/application/services/__test__/client.service.spec.ts
+++ src/modules/client/application/services/__test__/client.service.spec.ts
@@ -1,24 +1,79 @@
-import { ClientService } from '@/modules/client/application/services/client.service';
 import { Test } from '@nestjs/testing';
-import { ClientRepository } from '@/modules/client/application/ports/client.repository';
-import { PersistenceModule } from '@/shared/infrastructure/persistence/persistence.module';
-import { ClientController } from '@/modules/client/interface/http/client.controller';
-import { ConfigModule } from '@nestjs/config';
+import { ClientService } from '../client.service';
+import { ClientRepository } from '../../ports/client.repository';
+import { Client, CreateClientDomainDto } from '../../../domain/models/client';
+import * as crypto from 'node:crypto';
 
 describe('ClientService', () => {
   let service: ClientService;
+  let repository: jest.Mocked<ClientRepository>;
+
+  const mockPublicClient: Client = {
+    id: crypto.randomUUID(),
+    name: 'Test Public Client',
+    clientId: '',
+    clientSecret: '',
+    requirePkce: true,
+    allowedGrants: ['authorization_code', 'refresh_token', 'password'],
+    type: 'public',
+    allowedScopes: ['openid', 'profile', 'email', 'offline_access'],
+    redirectUris: [''],
+    settings: {
+      accessTokenTTL: 60 * 60 * 1000,
+      refreshTokenTTL: 60 * 60 * 24 * 7 * 1000,
+      tokenEndpointAuthMethod: 'client_secret_basic',
+    },
+    isActive: true,
+    createdAt: new Date(2024, 1, 1),
+    updatedAt: new Date(2024, 1, 1),
+  };
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      imports: [ConfigModule.forRoot({ isGlobal: true }), PersistenceModule],
-      providers: [ClientService, ClientRepository],
-      controllers: [ClientController],
+      providers: [
+        ClientService,
+        {
+          provide: ClientRepository,
+          useValue: {
+            findAllWithPagination: jest.fn(),
+            createOne: jest.fn(),
+            updateOne: jest.fn(),
+            findRows: jest.fn(),
+            deleteRows: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get(ClientService);
+    service = module.get<ClientService>(ClientService);
+    repository = module.get(ClientRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('createClient', () => {
+    it('should create public client without client secret', async () => {
+      repository.createOne.mockResolvedValue(mockPublicClient);
+
+      // Act
+      const mockPayload: CreateClientDomainDto = {
+        type: 'public',
+        name: 'Test Public Client',
+        allowedGrants: ['authorization_code', 'refresh_token', 'password'],
+        allowedScopes: ['openid', 'profile', 'email', 'offline_access'],
+      };
+
+      await service.createClient(mockPayload);
+
+      // Assert the repository received the correct payload
+      expect(repository.createOne).toHaveBeenCalledWith(
+        expect.objectContaining({
+          clientId: expect.any(String),
+          clientSecret: '',
+          name: 'Test Public Client',
+          type: 'public',
+          allowedGrants: mockPayload.allowedGrants,
+          allowedScopes: mockPayload.allowedScopes,
+        }),
+      );
+    });
   });
 });
