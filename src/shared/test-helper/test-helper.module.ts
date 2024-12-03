992c563cdbba469baf04c6ad4324b28e445e2c49
kingrayhan <rayhan095@gmail.com>
2024-12-03 22:25:34 +0600

    set env for test-helper-module

--- src/shared/test-helper/test-helper.module.ts
+++ src/shared/test-helper/test-helper.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PersistenceModule } from '@/shared/infrastructure/persistence/persistence.module';
 
+export const TEST_ENV = {
+  // DATABASE_URL: 'postgresql://test:test@localhost:5432/test_db',
+  JWT_SECRET: 'test-secret',
+  API_KEY: 'test-api-key',
+  NODE_ENV: 'test',
+  PORT: 3000,
+};
+
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PersistenceModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [() => TEST_ENV] }),
+    PersistenceModule,
+  ],
   exports: [ConfigModule, PersistenceModule],
 })
 export class TestHelperModule {}
