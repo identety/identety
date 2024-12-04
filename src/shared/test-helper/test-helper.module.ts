d67008fb97b56e273816ef1fdf60f184f7008a47
kingrayhan <rayhan095@gmail.com>
2024-12-04 09:49:37 +0600

    trying to fix github action e2e test

--- src/shared/test-helper/test-helper.module.ts
+++ src/shared/test-helper/test-helper.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PersistenceModule } from '@/shared/infrastructure/persistence/persistence.module';
-import process from 'node:process';
 
 @Module({
   imports: [
